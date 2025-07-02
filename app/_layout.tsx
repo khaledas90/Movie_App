57c6939b92c1e90391bf5139fb673581dff6e3cb
Khaled Ahmed <www.khaledah826@gmail.com>
2025-07-02 18:45:07 +0300

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,13 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { Stack } from "expo-router";
+import { I18nManager } from "react-native";
 import Toast from "react-native-toast-message";
 import "./global.css";
 export default function RootLayout() {
+  if (I18nManager.isRTL) {
+    I18nManager.allowRTL(false);
+    I18nManager.forceRTL(false);
+  }
   return (
     <AuthProvider>
       <Stack>
